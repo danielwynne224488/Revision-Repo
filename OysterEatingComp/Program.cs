45482c4616939e6474d0b57589a4fa5fbb7607c3
mtwki <mtwki@DANIELSTOWERPC>
2025-10-26 13:50:54 +0000

    Git Changes Innit

--- OysterEatingComp/Program.cs
+++ OysterEatingComp/Program.cs
@@ -2,18 +2,40 @@
 Console.WriteLine("How many competitors are there?");
 int competitorCount = int.Parse(Console.ReadLine());
 
-Names = new string[competitorCount];
-EatCount = new int[competitorCount];
+string[] Names = new string[competitorCount];
+int[] EatCount = new int[competitorCount];
 
 for (int i = 0; i < competitorCount; i++)
 {
     Console.WriteLine($"Enter the name of competitor #{i + 1}:");
     Names[i] = Console.ReadLine();
-}
-
-for (int i = 0; i < competitorCount; i++)
-{
     Console.WriteLine($"How many oysters did {Names[i]} eat?");
     EatCount[i] = int.Parse(Console.ReadLine());
 }
 
+for (int i = 0; i < Names.Length-1; i++)
+{
+    for (int j = i + 1; j < Names.Length; j++)
+    {
+        if (EatCount[i] < EatCount[j])
+        {
+            // Swap EatCount
+            int tempEat = EatCount[i];
+            EatCount[i] = EatCount[j];
+            EatCount[j] = tempEat;
+            // Swap Names
+            string tempName = Names[i];
+            Names[i] = Names[j];
+            Names[j] = tempName;
+        }
+    }
+}
+
+
+
+for (int i = 0; i < competitorCount; i++)
+{
+    Console.WriteLine($"{Names[i]} ate {EatCount[i]} oysters.");
+}
+
+Console.ReadLine();
