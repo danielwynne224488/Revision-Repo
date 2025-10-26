16c2d2b1e46d67e2fbff92a5e7640489f426e5f5
mtwki <mtwki@DANIELSTOWERPC>
2025-10-26 13:12:54 +0000

    Study for arrays

--- OysterEatingComp/Program.cs
+++ OysterEatingComp/Program.cs
@@ -1,4 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Oyster Eating Competition");
+﻿Console.WriteLine("Oyster Eating Competition");
 Console.WriteLine("How many competitors are there?");
 int competitorCount = int.Parse(Console.ReadLine());
+
+Names = new string[competitorCount];
+EatCount = new int[competitorCount];
+
+for (int i = 0; i < competitorCount; i++)
+{
+    Console.WriteLine($"Enter the name of competitor #{i + 1}:");
+    Names[i] = Console.ReadLine();
+}
+
+for (int i = 0; i < competitorCount; i++)
+{
+    Console.WriteLine($"How many oysters did {Names[i]} eat?");
+    EatCount[i] = int.Parse(Console.ReadLine());
+}
+
---
+++ ShoppingBasket/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Welcome to the shopping basket.");
+Console.WriteLine("The shopping basket can hold 5 items or less");
+
+string[] basketContents = { "Milk", "Bread", "Eggs", "Coffee", "Bacon" };
+
+bool checkout = false;
+do
+{
+    for (int i = 0; i < basketContents.Length; i++)
+    {
+        Console.WriteLine((i + 1) + ". " + basketContents[i]);
+    }
+
+    // Add your code here!
+
+    Console.WriteLine("Do you wish to replace an item in your basket? Put y for yes.");
+    if (Console.ReadLine() == "y")
+    {
+        Console.WriteLine("Which item number do you want to replace?");
+        int itemNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("What is the new item?");
+        string newItem = Console.ReadLine();
+        basketContents[itemNumber - 1] = newItem;
+    }
+
+    Console.WriteLine("Are you ready to check out? Answer y for yes");
+    string input = Console.ReadLine();
+    checkout = (input == "y");
+
+} while (!checkout);
+
+Console.Write("Your basket contains ");
+
+foreach (string item in basketContents)
+{
+    Console.Write(item + " ");
+}
