ed0a4ef055f32150b2afd63b108bd9c94f7f4023
mtwki <mtwki@DANIELSTOWERPC>
2025-10-26 12:37:58 +0000

    work?

---
+++ OysterEatingComp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Oyster Eating Competition");
+Console.WriteLine("How many competitors are there?");
+int competitorCount = int.Parse(Console.ReadLine());
