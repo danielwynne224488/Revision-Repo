41de04f83955ba7ac77962057a61b73340e5ea0d
DANIEL WYNNE <D.WYNNE-2023@hull.ac.uk>
2025-10-27 12:33:40 +0000

    Started "New" Summative

--- Programming Portfolio Calculator Results/Program.cs
+++ Programming Portfolio Calculator Results/Program.cs
@@ -3,4 +3,21 @@ using System;
 
 
 
+public class studentRecord
+{
+    public int ID;
+    public string FirstName;
+    public string LastName;
+    public int Challenge1;
+    public int Challenge2;
+    public int Challenge3;
+    public int Challenge4;
+    public int Challenge5;
+    public int Challenge6;
+    public int Challenge7;
+    public int Challenge8;
+}
+
 Console.WriteLine("Please select a file to load");
+Console.WriteLine("");
+string ChosenFile = Console.ReadLine();
