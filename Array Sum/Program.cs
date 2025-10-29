e197ab3d1d88a7ce9821f15c0fe594e277fb53e4
mtwki <mtwki@DANIELSTOWERPC>
2025-10-29 11:16:38 +0000

    Anxiety blah b;lah blah

--- Array Sum/Program.cs
+++ Array Sum/Program.cs
@@ -4,14 +4,14 @@ int SumValues(int[] pArray )
 {
     int total = 0;
 
-    foreach (int integer in pArray)
+    foreach (int sloop in pArray)
     {
-        total += integer;
+        total += sloop;
     }
 
 
     return total;
 }
 
-SumValues(pArray: new int[5] { 1, 2, 3, 4, 5 });
+SumValues(pArray: new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
 
