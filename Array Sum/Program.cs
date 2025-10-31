9f62ba3f32d6ef4738525b7575ec4ed1ee6ea31d
mtwki <mtwki@DANIELSTOWERPC>
2025-10-31 20:02:22 +0000

    Start of file rev

--- Array Sum/Program.cs
+++ Array Sum/Program.cs
@@ -11,7 +11,9 @@ int SumValues(int[] pArray )
 
 
     return total;
+    Console.WriteLine("The total is: " + total);
 }
 
 SumValues(pArray: new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
 
+Console.ReadLine();
--- ShoppingApp/Program.cs
+++ ShoppingApp/Program.cs
@@ -1,5 +1,30 @@
-﻿Console.WriteLine("How many items do you need to buy?");
+﻿using System;
+using System.IO;
+
+Console.WriteLine("How many items do you need to buy?");
 
 int numberOfItems = int.Parse(Console.ReadLine());
 
 string[] shoppingList = new string[numberOfItems];
+
+for (int j = 0; j < numberOfItems; j++)
+{
+    Console.WriteLine("What is item number " + (j + 1));
+    shoppingList[j] = Console.ReadLine();
+}
+
+string filename;
+
+Console.WriteLine("What file would you like to save your shopping list to?");
+
+filename = "../../../" + Console.ReadLine();
+
+using (StreamWriter writer = new StreamWriter(filename))
+{
+    writer.WriteLine("Shopping List " + DateTime.Now.Date);
+    foreach (string item in shoppingList)
+    {
+        writer.WriteLine(item);
+    }
+
+}
