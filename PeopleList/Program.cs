f0ba2d01317e110661e542c06557fa57fe74dbd5
mtwki <mtwki@DANIELSTOWERPC>
2025-11-20 16:16:02 +0000

    fr? fr?

--- PeopleList/Program.cs
+++ PeopleList/Program.cs
@@ -2,4 +2,27 @@
 using System.IO;
 
 Console.WriteLine("People List Program");
-string filename = "../../../people.txt";
+
+Person Julia = new Person();
+Julia.name = "blue";
+Julia.age = 2;
+Julia.gender = "Woman";
+Console.WriteLine(Julia.name);
+Console.WriteLine(Julia.age);
+Console.WriteLine(Julia.gender);
+
+Person Mark = new Person();
+Mark.name = Console.ReadLine();
+Mark.age = int.Parse(Console.ReadLine());
+Mark.gender = Console.ReadLine();
+Console.WriteLine(Mark.name);
+Console.WriteLine(Mark.age);
+Console.WriteLine(Mark.gender);
+
+
+class Person
+{
+    public string name;
+    public int age;
+    public string gender;
+}
