937524c13b4e3cffd2d6e25cbc135ce5b799833f
mtwki <mtwki@DANIELSTOWERPC>
2025-11-21 11:10:37 +0000

    GaydarRanking is now extremely misleading, oopsies

--- GaydarRanking/Program.cs
+++ GaydarRanking/Program.cs
@@ -1,13 +1,46 @@
 ﻿using System;
 using System.IO;
+using System.IO.Pipes;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
 
-robloxusergayranking();
+List<user> users = new List<user>();
 
-string robloxusergayranking(string robloxuser = "stepfrog", int gaydarrating = 8 );
+int i = 0;
+while (i < 1)
 {
-    Console.WriteLine("Please enter roblox username:");
-    string robloxuser = Console.ReadLine();
-    Console.WriteLine("Please enter their gayness");
-    int gaydarrating = int.Parse(Console.ReadLine());
+    Console.WriteLine("Do you wish to add a new user? y/n");
+    if (Console.ReadLine() == "n")
+    {
+        break;
+    }
+    else
+    {
+        users.Add(retrieval());
+    }
 
+}
+
+foreach (user u in users)
+{
+    Console.WriteLine("User birthday: " + u.birthday);
+    Console.WriteLine("User Name: " + u.userName);
+}
+
+Console.ReadLine();
+
+user retrieval()
+{
+    user New = new user();
+    Console.WriteLine("Enter user Name");
+    New.userName = Console.ReadLine();
+    Console.WriteLine("Enter user birthday (day/month/year)");
+    New.birthday = Console.ReadLine();
+    return New;
+}
+
+class user
+{
+    public string birthday;
+    public string userName;
 }
